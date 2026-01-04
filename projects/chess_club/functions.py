3da27cbaab04d2ac1c9d777e90ee02ce8a1a433e
niitse34 <niitse34@gmail.com>
2026-01-04 00:56:43 -0500

    yes

--- projects/chess_club/functions.py
+++ projects/chess_club/functions.py
@@ -2,17 +2,14 @@ import json
 import datetime as dt
 
 
-# Leer desde un archivo JSON (ruta configurable)
+# leer json
 def read_json(path="resources.json"):
     with open(path, "r", encoding="utf-8") as file:
         return json.load(file)
 
-# Escribir en un archivo JSON (ruta configurable)
+# escribir json
 def write_json(data, path="resources.json"):
     with open(path, "w", encoding="utf-8") as file:
         json.dump(data, file, ensure_ascii=False, indent=2)
 
-# Espacio para funciones de eventos especiales (implementación futura)
-# def evento_especial_puzzle_rush(...):
-#     pass
 
