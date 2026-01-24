d7fa0f742d5856deadc0777d4676a7904048ac5f
niitse34 <niitse34@gmail.com>
2026-01-24 10:24:46 -0500

    24/1 finishing main, implementing new restrictions

--- projects/chess_club/functions.py
+++ projects/chess_club/functions.py
@@ -1,15 +1,19 @@
 import json
 import datetime as dt
+import os
+from datetime import datetime, timedelta
 
 
-# leer json
-def read_json(path="resources.json"):
+#read json
+def read_json(path=None):
+    if path is None:
+        path = os.path.join(os.path.dirname(__file__), "resources.json")
     with open(path, "r", encoding="utf-8") as file:
         return json.load(file)
 
-# escribir json
-def write_json(data, path="resources.json"):
+#write json
+def write_json(data, path=None):
+    if path is None:
+        path = os.path.join(os.path.dirname(__file__), "resources.json")
     with open(path, "w", encoding="utf-8") as file:
         json.dump(data, file, ensure_ascii=False, indent=2)
-
-
